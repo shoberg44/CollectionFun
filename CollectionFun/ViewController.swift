999cab3e24e530c93ec0c96709759e21b5c68fe9
shoberg44 <steven.hoberg23@d155.org>
2022-09-09 07:51:59 -0500

    Initial Commit

---
+++ CollectionFun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionFun
+//
+//  Created by STEVEN HOBERG on 9/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CollectionFunUITests/CollectionFunUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CollectionFunUITestsLaunchTests.swift
+//  CollectionFunUITests
+//
+//  Created by STEVEN HOBERG on 9/9/22.
+//
+
+import XCTest
+
+class CollectionFunUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
